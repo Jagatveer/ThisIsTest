091c9db216c224b2f17ca2d503b444fdf6be13f2
Jagatveer Singh <jagatveer.singh@sourcefuse.com>
2016-04-27 12:27:22 +0530

    another attempt

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  * This is only a test file. LOL
+ * Another attempt
  * @package WordPress
  */
 
